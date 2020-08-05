2574a0073b0b1d90ff7701c4898446e9e29529fb
Eric Carraway <eric@ericcarraway.com>
2020-08-05 12:57:32 +0200

    publish.sh :parachute: bail if I'm not logged in to the correct account

--- publish.sh
+++ publish.sh
@@ -1,3 +1,18 @@
+username_expected="ericcarraway"
+username_actual=$(npm whoami)
+
+# verify that I'm logged in to the correct account
+if [ "$username_actual" != $username_expected ]; then
+  echo "publish.sh must be run as user: $username_expected"
+  echo "you are currently $username_actual"
+
+  exit 255
+fi
+
+echo "OK"
+
+exit;
+
 # because the repository has a scope,
 # we need to slightly adjust
 # the `npm publish` command
