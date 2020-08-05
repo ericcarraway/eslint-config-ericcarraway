e288d04c415744d5404a1c7abe8e4f7430a88843
Eric Carraway <eric@ericcarraway.com>
2020-08-05 12:38:08 +0200

    hoisted functions are OK 👍

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = {
   "rules": {
+     // hoisted functions (in the form of function
+     // declarations) are useful since they allow us to push
+     // cluttering implementation details away from (below)
+     // the usage of a function
+    "no-use-before-define": ["error", {
+      "functions": false
+    }],
+
     "operator-linebreak": ["error", "after", {
       "overrides": {
         "?": "before",
