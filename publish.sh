70eecfebe08ce6aa57a5c0e1e533ded532fb60aa
Eric Carraway <eric@ericcarraway.com>
2020-08-05 13:01:35 +0200

    publish.sh · verify that I'm using the correct registry

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 username_expected="ericcarraway"
 username_actual=$(npm whoami)
 
+registry_expected="https://registry.npmjs.org/"
+registry_actual=$(npm config get registry)
+
 # verify that I'm logged in to the correct account
 if [ "$username_actual" != $username_expected ]; then
   echo "publish.sh must be run as user: $username_expected"
@@ -9,9 +12,15 @@ if [ "$username_actual" != $username_expected ]; then
   exit 255
 fi
 
-echo "OK"
+# verify that I'm using the correct registry
+if [ "$registry_actual" != $registry_expected ]; then
+  echo "publish.sh must be run with registry: $registry_expected"
+  echo "your current registry is $registry_actual"
 
-exit;
+  exit 255
+fi
+
+echo "OK"
 
 # because the repository has a scope,
 # we need to slightly adjust
