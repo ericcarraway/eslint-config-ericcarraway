7bd8cd56cf1de02240ab75351353c5f94185c836
Eric Carraway <eric@ericcarraway.com>
2020-11-30 20:00:53 +0100

    Prettier

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 module.exports = {
-  "rules": {
-     // hoisted functions (in the form of function
-     // declarations) are useful since they allow us to push
-     // cluttering implementation details away from (below)
-     // the usage of a function
-    "no-use-before-define": ["error", {
-      "functions": false
-    }],
+  rules: {
+    // hoisted functions (in the form of function
+    // declarations) are useful since they allow us to push
+    // cluttering implementation details away from (below)
+    // the usage of a function
+    "no-use-before-define": [
+      "error",
+      {
+        functions: false,
+      },
+    ],
 
-    "operator-linebreak": ["error", "after", {
-      "overrides": {
-        "?": "before",
-        ":": "before"
-      }
-    }],
+    "operator-linebreak": [
+      "error",
+      "after",
+      {
+        overrides: {
+          "?": "before",
+          ":": "before",
+        },
+      },
+    ],
 
-    "quotes": ["error", "backtick"],
-  }
+    quotes: ["error", "backtick"],
+  },
 };
