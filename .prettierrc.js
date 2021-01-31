eab10e30809c4580e03e36857d0ed7e4dfe7bbca
Eric Carraway <eric@ericcarraway.com>
2021-01-31 16:55:58 +0100

    .prettierrc -> .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: `always`,
+  bracketSpacing: true,
+  insertPragma: false,
+  printWidth: 80,
+  quoteProps: `as-needed`,
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: `all`,
+  useTabs: false,
+};
