31811eabb9e2d16d0eb5b82fa89e53324220e7cf
Eric Carraway <eric@ericcarraway.com>
2021-01-31 16:49:20 +0100

    the lint configuration file should pass its own lint rules 🤔

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 module.exports = {
   rules: {
     // https://eslint.org/docs/rules/curly
-    curly: ['error', 'all'],
+    curly: [`error`, `all`],
 
     // disable this rule for now,
     // since it doesn't play will with Prettier
     'implicit-arrow-linebreak': [0],
 
-    'no-const-assign': 'error',
+    'no-const-assign': `error`,
 
     // hoisted functions (in the form of function
     // declarations) are useful since they allow us to push
     // cluttering implementation details away from (below)
     // the usage of a function
     'no-use-before-define': [
-      'error',
+      `error`,
       {
         functions: false,
       },
     ],
 
     'operator-linebreak': [
-      'error',
-      'after',
+      `error`,
+      `after`,
       {
         overrides: {
-          '?': 'before',
-          ':': 'before',
+          ':': `before`,
+          '?': `before`,
         },
       },
     ],
 
     'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
+      `error`,
+      { blankLine: `always`, next: `return`, prev: `*` },
     ],
 
-    quotes: ['error', 'backtick'],
+    quotes: [`error`, `backtick`],
 
     'sort-keys': [
-      'error',
-      'asc',
+      `error`,
+      `asc`,
       { caseSensitive: false, minKeys: 2, natural: true },
     ],
 
     // https://eslint.org/docs/rules/wrap-iife
-    'wrap-iife': ['error', 'inside'],
+    'wrap-iife': [`error`, `inside`],
   },
 };
