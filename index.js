3b04916ad1bcbb9fe96e917cd51491aa662d619c
Eric Carraway <eric@ericcarraway.com>
2021-11-23 12:03:59 +0100

    require a blank line before `throw` statements (similar to `return`)

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ module.exports = {
     'padding-line-between-statements': [
       `error`,
       { blankLine: `always`, next: `return`, prev: `*` },
+      { blankLine: `always`, next: `throw`, prev: `*` },
     ],
 
     'prefer-destructuring': [
