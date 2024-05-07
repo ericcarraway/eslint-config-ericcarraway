5e763f2a10d1c65ef78f1bf9d308c72512f9983d
Eric Carraway <eric@ericcarraway.com>
2024-05-07 18:07:10 -0400

    note RE: ESLint v8.53.0 deprecation of formatting rules

--- handled-by-prettier.js
+++ handled-by-prettier.js
@@ -2,6 +2,10 @@
  * A dictionary of rules set to 'off' because they are handled by Prettier.
  *
  * @see https://github.com/prettier/eslint-config-prettier/blob/main/index.js
+ *
+ * These rules were deprecated in ESLint v8.53.0 (November 2023).
+ * They will be removed in the future.
+ * @see https://eslint.org/blog/2023/10/deprecating-formatting-rules/
  */
 const handledByPrettier = [
   `function-paren-newline`,
