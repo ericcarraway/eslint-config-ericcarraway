4d07efb6ee3ae514f60f0685f9e23587ac9dbacf
Eric L Carraway <eric@ericcarraway.com>
2025-03-02 20:03:27 -0500

    import plugin rules

--- eslint.config.js
+++ eslint.config.js
@@ -178,11 +178,54 @@ export const disabledBaseRules = {
 export const importRules = {
   'import/consistent-type-specifier-style': [`error`, `prefer-top-level`],
   'import/default': `error`,
+  'import/dynamic-import-chunkname': [
+    `off`,
+    {
+      importFunctions: [],
+      webpackChunknameFormat: `[0-9a-zA-Z-_/.]+`,
+    },
+  ],
   'import/export': `error`,
+  'import/exports-last': `off`,
+  'import/extensions': [`off`, `ignorePackages`],
+  'import/first': `error`,
+  'import/group-exports': `off`,
+  'import/imports-first': `off`,
+  'import/max-dependencies': [
+    `off`,
+    {
+      max: 10,
+    },
+  ],
   'import/named': `error`,
   'import/namespace': `error`,
-  'import/no-default-export': [`error`],
-  'import/no-duplicates': `warn`,
+  'import/newline-after-import': `error`,
+  'import/no-absolute-path': `error`,
+  'import/no-amd': `error`,
+  'import/no-anonymous-default-export': [
+    `off`,
+    {
+      allowAnonymousClass: false,
+      allowAnonymousFunction: false,
+      allowArray: false,
+      allowArrowFunction: false,
+      allowLiteral: false,
+      allowObject: false,
+    },
+  ],
+  'import/no-commonjs': `off`,
+  'import/no-cycle': [
+    `error`,
+    {
+      allowUnsafeDynamicCyclicDependency: false,
+      ignoreExternal: false,
+      maxDepth: `∞`,
+    },
+  ],
+  'import/no-default-export': `error`,
+  'import/no-deprecated': `off`,
+  'import/no-duplicates': `error`,
+  'import/no-dynamic-require': `error`,
   'import/no-extraneous-dependencies': [
     `error`,
     {
@@ -196,10 +239,63 @@ export const importRules = {
       ],
     },
   ],
-  'import/no-named-as-default': `warn`,
-  'import/no-named-as-default-member': `warn`,
-  'import/no-unresolved': `error`,
+  'import/no-import-module-exports': [
+    `error`,
+    {
+      exceptions: [],
+    },
+  ],
+  'import/no-internal-modules': [
+    `off`,
+    {
+      allow: [],
+    },
+  ],
+  'import/no-mutable-exports': `error`,
+  'import/no-named-as-default': `error`,
+  'import/no-named-as-default-member': `error`,
+  'import/no-named-default': `error`,
+  'import/no-named-export': `off`,
+  'import/no-namespace': `off`,
+  'import/no-nodejs-modules': `off`,
+  'import/no-relative-packages': `error`,
+  'import/no-relative-parent-imports': `off`,
+  'import/no-restricted-paths': `off`,
+  'import/no-self-import': `error`,
+  'import/no-unassigned-import': `off`,
+  'import/no-unresolved': [
+    `off`,
+    {
+      caseSensitive: true,
+      caseSensitiveStrict: false,
+      commonjs: true,
+    },
+  ],
+  'import/no-unused-modules': [
+    `off`,
+    {
+      ignoreExports: [],
+      missingExports: true,
+      unusedExports: true,
+    },
+  ],
+  'import/no-useless-path-segments': [
+    `error`,
+    {
+      commonjs: true,
+    },
+  ],
+  'import/no-webpack-loader-syntax': `error`,
+  'import/order': [
+    `error`,
+    {
+      distinctGroup: true,
+      groups: [[`builtin`, `external`, `internal`]],
+      warnOnUnassignedImports: false,
+    },
+  ],
   'import/prefer-default-export': `off`,
+  'import/unambiguous': `off`,
 };
 
 export const simpleImportSortRules = {
